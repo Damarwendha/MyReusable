4452ba1881b9d2261236c1f5704c76c069c8f48c
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-31 05:10:08 +0700

    changing deprecated substr to substring

--- src/utils/formatRupiah.ts
+++ src/utils/formatRupiah.ts
@@ -2,8 +2,8 @@ function formatRupiah(angka: string | number, prefix: string): string {
   const number_string = angka.toString().replace(/[^,\d]/g, "");
   const split = number_string.split(",");
   const sisa = split[0].length % 3;
-  let rupiah = split[0].substr(0, sisa);
-  const ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+  let rupiah = split[0].substring(0, sisa);
+  const ribuan = split[0].substring(sisa).match(/\d{3}/gi);
 
   // tambahkan titik jika yang di input sudah menjadi angka ribuan
   if (ribuan) {
