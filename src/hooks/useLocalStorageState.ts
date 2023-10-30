a226445ba65f073ab317720397af047881152426
Sien Khumaen Damarwendha <sienkhumaen@gmail.com>
2023-10-31 04:33:23 +0700

    add not null operator

--- src/hooks/useLocalStorageState.ts
+++ src/hooks/useLocalStorageState.ts
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 
 export function useLocalStorageState(key: string, initialValue: any) {
   const [value, setValue] = useState(
-    () => JSON.parse(localStorage.getItem(key)) || initialValue
+    () => JSON.parse(localStorage.getItem(key)!) || initialValue
   );
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
-
+  
   return [value, setValue];
 }
