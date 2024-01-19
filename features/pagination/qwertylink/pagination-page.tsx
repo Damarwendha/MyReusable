aad0c22df7a191d2e972bb802040798a9a6550dd
Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2024-01-20 00:40:56 +0700

    Update pagination-page.tsx

--- features/pagination/qwertylink/pagination-page.tsx
+++ features/pagination/qwertylink/pagination-page.tsx
@@ -1,21 +1,36 @@
+const payments: IPayment[] = [
+  {
+    payment_id: 14,
+    username: "GraceMiller",
+    status: "cancelled",
+    method: "PayPal",
+    amount: 78.9012,
+    date: "2024-01-06 19:35:25",
+  },
+]
+
 function ProofPayment({ searchParams }: ProofPaymentProps) {
   const page = searchParams.page ?? "1";
 
-  const data_per_page = 10;
+  const DATA_EACH_PAGE = 10;
 
+  // i want to show the latest data first... thats why this variable exits
   const latestId = payments.reduce((acc, { payment_id }) => {
     return payment_id > acc ? payment_id : acc;
   }, 0);
 
-  const endId = latestId - (+page - 1) * data_per_page;
+  // ex. endId = 20
+  const endId = latestId - (+page - 1) * DATA_EACH_PAGE;
 
-  const startId = endId - data_per_page;
+  // ex. startId = 10
+  const startId = endId - DATA_EACH_PAGE;
 
+  // entries show data from startId to endId
   const entries = payments
     .filter(({ payment_id }) => payment_id > startId && payment_id <= endId)
     .sort(({ payment_id: a }, { payment_id: b }) => b - a);
 
-  const total_pages = latestId / data_per_page;
+  const total_pages = latestId / DATA_EACH_PAGE;
 
   return (
     <>
