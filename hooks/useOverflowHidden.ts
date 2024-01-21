f171496bc3636f1558220978363e0a96dac169c5
Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2024-01-21 21:05:30 +0700

    Update useOverflowHidden.ts

--- hooks/useOverflowHidden.ts
+++ hooks/useOverflowHidden.ts
@@ -1,19 +1,27 @@
-import { useEffect } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
 
 function useOverflowHidden(
   setIsHidden: boolean = true,
-  deps: any[] = []
-): void {
+  deps: React.DependencyList = []
+) {
   useEffect(() => {
-    const bodyStyle = document.querySelector('body')!.style;
+    const bodyElement = document.querySelector("body") as HTMLElement;
+
+    // Q: Why didn't you simply use 'overflow: hidden' instead?
+    // A: The reason for this is that overflow: hidden makes the site jump and take up the area where the scroll was.
     if (setIsHidden) {
-      bodyStyle.overflow = 'hidden';
+      bodyElement.style.position = "fixed";
+      bodyElement.style.overflowY = "scroll";
+      bodyElement.style.width = "100%";
     } else {
-      bodyStyle.overflow = 'visible';
+      bodyElement.style.position = "unset";
+      bodyElement.style.overflowY = "unset";
+      bodyElement.style.width = "unset";
     }
 
     return () => {
-      bodyStyle.overflow = 'visible';
+      bodyElement.style.overflow = "visible";
     };
   }, deps);
 }
