98a57f8b23479f4e10316913ea7ab797393dc821
Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2024-01-24 03:57:23 +0700

    i give up 😂

--- hooks/useOverflowHidden.ts
+++ hooks/useOverflowHidden.ts
@@ -1,50 +1,23 @@
-import { useEffect, useState } from "react";
-
-// Q: Why didn't you simply use 'overflow: hidden' instead?
-// A: The reason for this is that because overflow: hidden makes the site jump and take up the area where the scroll was.
+import { useEffect } from "react";
 
 function useOverflowHidden(
   isHiddenOverflow: boolean,
   deps: React.DependencyList = []
 ) {
-  const [scrollTopPosition, setScrollTopPosition] = useState(
-    document.documentElement.scrollTop
-  );
-
   useEffect(() => {
-    if (isHiddenOverflow) {
-      setScrollTopPosition(document.documentElement.scrollTop);
-    }
-  }, [isHiddenOverflow]);
-
-  useEffect(() => {
-    const bodyElement = document.querySelector("body") as HTMLElement;
+    const bodyElement = document.querySelector("html") as HTMLElement;
 
     if (isHiddenOverflow) {
-      bodyElement.style.top = String(-scrollTopPosition) + "px";
-      bodyElement.style.inlineSize = "100%";
-      bodyElement.style.position = "fixed";
-      bodyElement.style.overflowY = "scroll";
-      bodyElement.style.width = "100%";
+      bodyElement.style.overflowY = "hidden";
     } else {
-      // set back scrollPosition when modal closed
-      bodyElement.style.top = String(-scrollTopPosition) + "px";
-      document.documentElement.scrollTop = scrollTopPosition;
-
-      bodyElement.style.inlineSize = "unset";
-      bodyElement.style.position = "unset";
       bodyElement.style.overflowY = "unset";
-      bodyElement.style.width = "unset";
     }
 
     return () => {
-      bodyElement.style.inlineSize = "unset";
-      bodyElement.style.position = "unset";
       bodyElement.style.overflowY = "unset";
-      bodyElement.style.width = "unset";
     };
     /* eslint-disable react-hooks/exhaustive-deps */
-  }, [...deps, scrollTopPosition, isHiddenOverflow]);
+  }, [...deps, isHiddenOverflow]);
 }
 
 export { useOverflowHidden };
