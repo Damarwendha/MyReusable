cf4997fa915c9630d0c60ab26c1cf20bc0f04cab
S.K. Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2024-02-27 20:02:55 +0700

    Update Breadcrumb.tsx

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,4 +1,84 @@
+// CN
+// V2
+"use client";
+
+import { usePathname } from "next/navigation";
+import { ChevronRight, Home } from "lucide-react";
+import { upperCaseFirstLett } from "@/lib/utils";
+import * as React from "react";
+import { Link } from "@/lib/router-events";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface IAddHref {
+  pathname: string;
+  href: string;
+}
+
+function BreadcrumbUI({
+  title,
+  addHref,
+}: {
+  title?: string;
+  addHref?: IAddHref;
+}) {
+  const pathname = usePathname();
+
+  const eachPathWrappedWithArray = pathname?.split("/");
+  // Current Path
+  const currPath = upperCaseFirstLett(
+    eachPathWrappedWithArray?.at(eachPathWrappedWithArray?.length - 1) as string
+  );
+
+  // All path but without the current path
+  const allPathExceptCurrPath = eachPathWrappedWithArray
+    ?.filter((_, i) => i !== 0 && i !== eachPathWrappedWithArray?.length - 1)
+    ?.map((path) => upperCaseFirstLett(path));
+
+  return (
+    <header className="flex flex-col">
+      <span className="font-semibold text-muted-foreground">
+        {title ? title : upperCaseFirstLett(currPath as string)}
+      </span>
+      <div className="flex items-center gap-1 text-xs font-bold text-muted-foreground opacity-60">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link className="hover-fade" href="/">
+              <Home size={13} />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">Home</TooltipContent>
+        </Tooltip>
+
+        {allPathExceptCurrPath.map((str, i) => (
+          <React.Fragment key={str}>
+            {i === 0 ? <ChevronRight size={10} /> : null}
+
+            {/* if addHref exits, render Link */}
+            {addHref?.pathname === str ? (
+              <Link className="hover:underline" href={addHref.href}>
+                {str}
+              </Link>
+            ) : (
+              <span>{str}</span>
+            )}
+            <ChevronRight size={10} />
+          </React.Fragment>
+        ))}
+        <div>{title ? title : currPath}</div>
+      </div>
+    </header>
+  );
+}
+
+export default BreadcrumbUI;
+
+
 // TAILWIND
+// V1
 "use client";
 
 import { usePathname } from "next/navigation";
